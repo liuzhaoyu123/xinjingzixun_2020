47e07bf9b99c355f6e4786a5573846c80edd3103
刘兆宇 <1965255374@qq.com>
2020-10-08 20:20:10 +0800

    4-将main.py中的视图函数迁移到views/index.py中，且通过蓝图，让flask对象能够找到这些视图函数，从而当路由匹配成功后，能够调用views/index.py中的视图函数

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 from flask import Flask
+from views import index_blu
 
 # 创建flask应用对象
 app = Flask(__name__)
 
-
-@app.route("/")
-def index():
-    return "我是zz主页"
-
+# 用app对象注册蓝图
+app.register_blueprint(index_blu)
 
 if __name__ == '__main__':
     app.run(port=8899)
